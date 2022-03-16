98a8525328bbd642d9280d21789a4bced256de89
admin <admin@Bcalab4-m12>
2022-03-16 10:42:21 +0530

    Added a method display

--- Lab2/src/Student.java
+++ Lab2/src/Student.java
@@ -1,6 +1,18 @@
 
 public class Student {
-
+	String name;
+	String Roll_num;
+	Student()
+	{
+		this.name=name;
+		this.Roll_num=Roll_num;
+		
+		
+	}
+void show()
+{
+	
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
